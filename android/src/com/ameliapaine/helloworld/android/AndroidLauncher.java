649fb1b15b47e4ca61b4c653834b08e87b86d512
ameliapaine <amelia.infinity@gmail.com>
2015-06-14 18:21:58 -0400

    first commit

---
+++ android/src/com/ameliapaine/helloworld/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.ameliapaine.helloworld.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.ameliapaine.helloworld.Main;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Main(), config);
+	}
+}
